830f87c0a59d1393e1f5c7a480688f78f5d9d957
rhivent <zzzzjrjr@gmail.com>
2020-01-18 20:11:10 +0700

    Updates

---
+++ precache-manifest.9f23109b6a9810d032bf03d17a15c1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e17fbffbeb325d48f4f1eff6812b3d5",
+    "url": "/rolonx/index.html"
+  },
+  {
+    "revision": "3f181b099412f0eaa5e4",
+    "url": "/rolonx/static/css/main.f857a3b4.chunk.css"
+  },
+  {
+    "revision": "8719ffd116bb226898da",
+    "url": "/rolonx/static/js/2.7bf7d871.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/rolonx/static/js/2.7bf7d871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f181b099412f0eaa5e4",
+    "url": "/rolonx/static/js/main.eefe5ef6.chunk.js"
+  },
+  {
+    "revision": "ea546d2d1b74232e672d",
+    "url": "/rolonx/static/js/runtime-main.a1f9661c.js"
+  }
+]);
